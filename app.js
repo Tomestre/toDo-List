e7f8682be615882b995be2eefd5d1f20e8f9fa44
Tomestre <gabrielveleci@gmail.com>
2022-12-11 00:59:12 -0300

    atualizando projeto

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+$('#add').on( "change", function( event ) {
+        event.preventDefault();
 
+       $('#lista').append(
+        `<li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${$('#input').val()}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>`)
+    
+    
+  });
